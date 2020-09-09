55e0536efe6c6314a55bd6419a12bb79cc24b75e
alexander <zeimedee@gmail.com>
2020-09-09 15:17:43 +0000

    number 6

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{useState,useEffect} from 'react';
+import './App.css'
+import axios from 'axios'
+import {Container, Row, Col} from 'react-grid-system'
 
 function App() {
+  const [data, setData] = useState([]);
+
+
+  const getData=()=>{
+    axios.get('https://corona.lmao.ninja/v2/countries/gh')
+         .then((data)=>{
+            setData(data.data)
+         })
+  }
+
+  useEffect(() => {
+    getData()
+    
+  }, [data])
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      <Container>
+        <Row>
+          <Col className='case center'> Cases <br/>{data.cases}</Col>
+          <Col className='death center'> Deaths <br/>{data.deaths}</Col>
+          <Col className='active center'>Active <br/>{data.active}</Col>
+          <Col className='recovered center'>Recovered <br/>{data.recovered}</Col>
+        </Row>
+      </Container>
+      
     </div>
   );
 }
